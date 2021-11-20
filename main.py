732733f714e56a054db0c369e16b946c69444f39
saulrega <p4n73ra@gmail.com>
2021-11-19 20:25:11 -0600

    models_tweets

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Python
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 
 
@@ -42,7 +42,15 @@ class User(UserBase):
 
 
 class Tweet(BaseModel):
-   pass
+   tweet_id: UUID = Field(...)
+   content: str = Field(
+      ...,
+      min_length=1,
+      max_length=290,
+   )
+   created_at: datetime = Field(default=datetime.now())
+   update_at: Optional[datetime] = Field(default=None)
+   by: User = Field(...)
 
 
 
