97ed098709ead0c11f3e84fbcc1c191579955125
saulrega <p4n73ra@gmail.com>
2021-11-22 15:55:44 -0600

    users_show_all

--- main.py
+++ main.py
@@ -124,8 +124,24 @@ def login():
    tags=["Users"]
 )
 def show_all_users():
-   pass
+   """
+   Singup a user
 
+   This path operation shows all users in the app
+
+   Parameters:
+      - 
+
+   Returns a json list with all users in the app, whit the following keys:
+      -user_id: UUID
+      -email: Emailstr
+      -first_name: str
+      -last_name: str
+      -birth_date: date
+   """
+   with open("users.json", "r", encoding="utf-8") as f:
+      results = json.loads(f.read())
+      return results
 
 ### Delete a user
 @app.get(
